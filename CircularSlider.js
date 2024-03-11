88235f0ba170ef225a64636ef49af64851e4c005
Vlad <vlado@strackov.ski>
2024-03-11 23:01:49 +0100

    add slider class with method stubs

---
+++ CircularSlider.js
@@ -0,0 +1,46 @@
+class CircularSlider {
+    constructor({container, sliders}) {
+        this.container = container;
+        this.sliders = sliders;
+    }
+
+    /**
+     * Draw all sliders as configured in options
+     */
+    draw() {
+
+    }
+
+    /**
+     * Draw slider
+     */
+    drawSlider() {
+
+    }
+
+    /**
+     * Draw handle
+     *
+     * @param slider
+     */
+    drawHandle(slider) {
+    }
+
+    /**
+     * Draw legend box
+     */
+    drawLegend() {
+    }
+
+    /**
+     * Update slider on mouse interaction
+     */
+    updateSlider() {
+    }
+
+    /**
+     * Update legend
+     */
+    updateLegend() {
+    }
+}
